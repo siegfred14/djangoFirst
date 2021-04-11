0db55765f3fe26377f49ad3728e9928601ec7822
Siegfred Samson <siegfredsamson@gmail.com>
2021-04-11 10:42:34 +0100

    myNewDjangoApp/views.py modified

--- myNewDjangoApp/views.py
+++ myNewDjangoApp/views.py
@@ -3,4 +3,5 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello and Yaayyy")
+    return HttpResponse("Hello! Welcome to my first Django App! "
+                        "Yaayyy!!!")
